26dd858d80bbf1a831832ef77f140a94bb5566bb
felipe <felipe.baleche2@gmail.com>
2024-05-26 14:54:22 -0300

    added waiter logic

---
+++ entities/waiter.py
@@ -0,0 +1,25 @@
+from random import uniform
+from time import sleep
+
+
+class Waiter:
+
+    def __init__(self, orders, condition, id):
+        self.id = id
+        self.orders = orders
+        self.condition = condition
+        self.MAX_ORDERS = 10
+
+    def make_order(self):
+        for i in range(self.MAX_ORDERS):
+            sleep(uniform(0.5, 2.0))
+
+            with self.condition:
+
+                while len(self.orders) <= 4:
+                    self.condition.wait()
+
+                order = f"Order {i} - Waiter {self.id}"
+                self.orders.append(order)
+                print(f"Waiter {self.id} realized | {order} |")
+                self.condition.notify_all()
---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+
+    orders = []
+
+
+if __name__ == "__main__":
+    main()
