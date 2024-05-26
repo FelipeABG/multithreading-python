bb41e51960547e465aa280dc6335336f3dacd6db
felipe <felipe.baleche2@gmail.com>
2024-05-26 16:28:00 -0300

    added most of main logic

--- entities/waiter.py
+++ entities/waiter.py
@@ -4,7 +4,8 @@ from time import sleep
 
 class Waiter:
 
-    def __init__(self, orders, condition, id):
+    def __init__(self, id, orders, condition, stopped_working):
+        self.stopped_working = stopped_working
         self.id = id
         self.orders = orders
         self.condition = condition
@@ -16,10 +17,13 @@ class Waiter:
 
             with self.condition:
 
-                while len(self.orders) <= 4:
+                while len(self.orders) >= 4:
                     self.condition.wait()
 
                 order = f"Order {i} - Waiter {self.id}"
                 self.orders.append(order)
                 print(f"Waiter {self.id} realized | {order} |")
                 self.condition.notify_all()
+
+        with self.condition:
+            self.stopped_working.append(self.id)
--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import threading
+from entities.waiter import Waiter
+from entities.chef import Chef
+
+
 def main():
 
     orders = []
+    condition = threading.Condition()
+    threads = []
+
+    waiters = []
+    chefs = []
+
+    for i in range(5):
+        waiters.append(Waiter(i, orders, condition))
+    for i in range(1):
+        chefs.append(Chef(i, orders, condition))
+
+    for waiter in waiters:
+        t = threading.Thread(target=waiter.make_order)
+        t.start()
+        threads.append(t)
+
+    for chef in chefs:
+        t = threading.Thread(target=chef.prepare_order)
+        t.start()
+        threads.append(t)
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
